70b0749636b14cec9383fde85fea2e16d6bb3f77
nami <almost.nami@gmail.com>
2022-09-18 16:50:37 +0900

    Second Commit

---
+++ src/main/java/org/zerock/controller/HomeController.java
@@ -0,0 +1,15 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String goHome(Model model) {
+        model.addAttribute("serverTime", new java.util.Date());
+        return "home";
+    }
+}
